3c316b4f872af58daf366525cbf2eb00f1dd7c96
Ram mohan <ramamohanraobotla12@gmail.com>
2024-07-10 00:10:04 +0530

    add a rout to delete a product

--- index.js
+++ index.js
@@ -14,39 +14,60 @@ mongoose.connect('mongodb://localhost:27017/farmStand', { useNewUrlParser: true,
     console.log(err);
 })
 
+
 app.set('views',path.join(__dirname,'views'))
 app.set('view engine','ejs')
+
+
 app.use(express.urlencoded({extended:true}))
 app.use(methodOverride('_method'))
 
+
 const categories=['fruit','vegetable','dairy']
+
 
 app.get('/products',async(req,res)=>{
     const products=await Product.find({});
     res.render("products/index.ejs",{products});
 })
+
+
 app.get('/products/new',(req,res)=>{
     res.render("products/new",{categories});
 })
+
+
 app.post('/products',async(req,res)=>{
     const product=new Product(req.body);
     await product.save();
     res.redirect(`/products/${product._id}`);
 })
+
+
 app.get('/products/:id',async(req,res)=>{
     const {id}=req.params;
     const product=await Product.findById(id);
     res.render("products/show",{product});
 })
+
+
 app.get('/products/:id/edit',async(req,res)=>{
     const {id}=req.params;
     const product=await Product.findById(id);
     res.render("products/edit",{product,categories});
 })
+
+
 app.put('/products/:id',async(req,res)=>{
     const {id}=req.params;
     const product=await Product.findByIdAndUpdate(id,req.body,{runValidators:true, new:true} );
     res.redirect(`/products/${product._id}`)
+})
+
+app.delete('/products/:id',async(req,res)=>{
+    const {id}=req.params;
+    const deletedProduct=await Product.findByIdAndDelete(id);
+    res.redirect('/products');
 })
 app.listen(3000,()=>{
     console.log("app is running on port 3000")
