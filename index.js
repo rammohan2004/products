522ac51d1d48fd745bc64e13fae94eebc00b0c55
Ram mohan <ramamohanraobotla12@gmail.com>
2024-07-09 23:12:28 +0530

    add a router to create a newproduct

--- index.js
+++ index.js
@@ -14,13 +14,20 @@ mongoose.connect('mongodb://localhost:27017/farmStand', { useNewUrlParser: true,
 
 app.set('views',path.join(__dirname,'views'))
 app.set('view engine','ejs')
-
+app.use(express.urlencoded({extended:true}))
 
 app.get('/products',async(req,res)=>{
     const products=await Product.find({});
     res.render("products/index.ejs",{products});
 })
-
+app.get('/products/new',(req,res)=>{
+    res.render("products/new");
+})
+app.post('/products',async(req,res)=>{
+    const product=new Product(req.body);
+    await product.save();
+    res.redirect(`/products/${product._id}`);
+})
 app.get('/products/:id',async(req,res)=>{
     const {id}=req.params;
     const product=await Product.findById(id);
