f72fdecd7ae27f2b1ba3423248cde09dc7684100
Ram mohan <ramamohanraobotla12@gmail.com>
2024-07-09 23:41:56 +0530

    add a router to update a product

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const path=require('path');
 const app=express()
 const Product=require('./models/product')
 const mongoose=require('mongoose')
+var methodOverride = require('method-override')
+
 mongoose.connect('mongodb://localhost:27017/farmStand', { useNewUrlParser: true, useUnifiedTopology: true })
 .then(()=>{
     console.log("connection open")
@@ -15,7 +17,7 @@ mongoose.connect('mongodb://localhost:27017/farmStand', { useNewUrlParser: true,
 app.set('views',path.join(__dirname,'views'))
 app.set('view engine','ejs')
 app.use(express.urlencoded({extended:true}))
-
+app.use(methodOverride('_method'))
 app.get('/products',async(req,res)=>{
     const products=await Product.find({});
     res.render("products/index.ejs",{products});
@@ -32,6 +34,16 @@ app.get('/products/:id',async(req,res)=>{
     const {id}=req.params;
     const product=await Product.findById(id);
     res.render("products/show",{product});
+})
+app.get('/products/:id/edit',async(req,res)=>{
+    const {id}=req.params;
+    const product=await Product.findById(id);
+    res.render("products/edit",{product});
+})
+app.put('/products/:id',async(req,res)=>{
+    const {id}=req.params;
+    const product=await Product.findByIdAndUpdate(id,req.body,{runValidators:true, new:true} );
+    res.redirect(`/products/${product._id}`)
 })
 app.listen(3000,()=>{
     console.log("app is running on port 3000")
