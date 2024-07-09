d62d26af12b18f1f6ad407393ff43ef783470db5
Ram mohan <ramamohanraobotla12@gmail.com>
2024-07-09 22:06:09 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express=require('express')
+const path=require('path');
+const app=express()
+
+const mongoose=require('mongoose')
+mongoose.connect('mongodb://localhost:27017/shopApp', { useNewUrlParser: true, useUnifiedTopology: true })
+.then(()=>{
+    console.log("connection open")
+})
+.catch(err=>{
+    console.log("error");
+    console.log(err);
+})
+
+app.set('views',path.join(__dirname,'views'))
+app.set('view engine','ejs')
+app.listen(3000,()=>{
+    console.log("app is running on port 3000")
+})
+
+app.get('/',(req,res)=>{
+    res.render("index");
+})
